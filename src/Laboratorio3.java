989eaceef1d44ed13ca8f300c83c9701e2b8e3e7
alessandro <sbiliphone@icloud.com>
2022-05-02 15:23:06 +0200

    Inizio problema 12

--- src/Laboratorio3.java
+++ src/Laboratorio3.java
@@ -1,6 +1,10 @@
 public class Laboratorio3 {
     public static void main(String[] args){
-        josephus(7);
+        RoundTable roundTable = new RoundTable(4);
+        roundTable.serveNeighbour();
+        roundTable.passJug();
+        System.out.println(roundTable.knightWithJug());
+        //josephus(7);
     }
 
     private static IntSList josephus(int num){
